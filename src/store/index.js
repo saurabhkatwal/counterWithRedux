8fcabbd7510a3c044b4e33b90d2a9c58aae68c58
saurabh <saurabhkatwal@gmail.com>
2022-10-09 21:11:33 +0530

    removed unnecessary code

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,13 @@ import { createStore } from "redux";
 const counterReducer=(state={counter:0,maxValue:0,steps:1},action)=>{
 if(action.type==="increment"){
     return {...state,
-        counter:(state.counter+state.steps<=state.maxValue)?(state.counter+state.steps):(state.maxValue),
-        // maxValue:state.maxValue
-
+        counter:(state.counter+state.steps<=state.maxValue)?(state.counter+state.steps):(state.maxValue)
     }
 }
 if(action.type==="decrement"){
     return {
         ...state,
-        counter:(state.counter-state.steps>=0)?(state.counter-state.steps):(0),
-        // maxValue:state.maxValue
+        counter:(state.counter-state.steps>=0)?(state.counter-state.steps):(0)
     }
 }
 if(action.type==="reset"){
@@ -19,7 +16,6 @@ if(action.type==="reset"){
         counter:0,
         steps:1,
         maxValue:0
-        // maxValue:state.maxValue
     }
 }
 if(action.type==="setStep"){
